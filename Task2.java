7f4d199bc0ded9287dcc8dd212caf0ac0eb5afa1
michaeljohnosorio2-droid <michael.john.osorio2@gmail.com>
2026-03-26 11:26:17 +0800

    created a program that would print  H3110 w0r1d 2.0 true

---
+++ Task2.java
@@ -0,0 +1,20 @@
+
+public class Task2 {
+
+  public static void main(String[] args) {
+//    byte, short, int, float, boolean and char
+//     H3110 w0r1d 2.0 true
+    byte firstVar = 3;
+    short secondVar = 1;
+    int thirdVar = 0;
+    char fourthVar = 'd';
+    float fifthVar = 2f;
+    boolean sixthVar = true;
+
+    System.out.println("H"+ firstVar + secondVar + secondVar +
+            thirdVar + " w"+thirdVar+"r"+secondVar+fourthVar+" "+fifthVar+" "+sixthVar);
+
+
+  }
+
+}
