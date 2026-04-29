68041188a30298aefe53ad97010dfe4b61f5a235
michaeljohnosorio2-droid <michael.john.osorio2@gmail.com>
2026-04-29 11:09:03 +0800

    finished task4

--- Task4.java
+++ Task4.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Task4 {
-    public static void main(String[] args) {
+    public static void main() {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter a string: ");
